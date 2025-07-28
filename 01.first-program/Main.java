7f04dd43db8f89997c28758236654cfcad6b3114
mishajib <mishajib222@gmail.com>
2025-07-28 10:17:01 +0600

    Initial commit

---
+++ 01.first-program/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Java World!");
+
+        String name = "MD. MAZHARUL ISLAM (SHAJIB)";
+        int age = 28;
+        String country = "Bangladesh";
+
+        System.out.println("My name is " + name);
+        System.out.println("I'm " + age + " years old");
+        System.out.println("I'm from " + country);
+    }
+}
---
+++ 02.variables/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        String studentName = "John Doe";
+        int studentId = 25;
+        int studentAge = 20;
+        float studentFee = 100.25f;
+        char studentGrade = 'B';
+        System.out.println("===============================");
+        System.out.println("Printing Student Information:");
+        System.out.println("===============================");
+        System.out.println("Student Name: " + studentName);
+        System.out.println("Student ID: " + studentId);
+        System.out.println("Student Age: " + studentAge);
+        System.out.println("Student Fee: " + studentFee);
+        System.out.println("Student Grade: " + studentGrade);
+        System.out.println("======================================");
+        System.out.println("Completed Print Student Information:");
+        System.out.println("======================================");
+    }
+}
